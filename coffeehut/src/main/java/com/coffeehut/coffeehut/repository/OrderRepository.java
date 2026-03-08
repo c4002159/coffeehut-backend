f1ecdbf63f0ec4c8e306f941396ce11e52651685
KaiserZheng667 <2302090168@qq.com>
2026-03-08 21:00:30 +0000

    Add methods to find orders by customer name and archive status

--- coffeehut/src/main/java/com/coffeehut/coffeehut/repository/OrderRepository.java
+++ coffeehut/src/main/java/com/coffeehut/coffeehut/repository/OrderRepository.java
@@ -1,9 +1,17 @@
 package com.coffeehut.coffeehut.repository;
-
 import com.coffeehut.coffeehut.model.Order;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    // 根据姓名查询订单
+
+    List<Order> findByCustomerName(String customerName);
+
+    // 查询未归档订单
     List<Order> findByIsArchivedFalse();
+    List<Order> findByIsArchivedTrue();
 }
