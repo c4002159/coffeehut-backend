2b4c37f4fd8a6ad41ba3fbfd9d8d23d7e2e6baad
Zhangkaichen role6 <2936238734@qq.com>
2026-04-03 16:10:31 +0100

    role6: add loyalty backend

---
+++ coffeehut/src/main/java/com/coffeehut/coffeehut/sixZhangkaichen/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.coffeehut.coffeehut.sixZhangkaichen;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
